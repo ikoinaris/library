f4ebd9db35994a557e1824e662fb2ed570b77143
ikoinaris <g.koinaris@hotmail.com>
2020-12-02 12:40:19 +0100

    library events producer first commit

---
+++ library-events-producer/src/main/java/com/apachekafka/libraryeventsproducer/domain/LibraryEvent.java
@@ -0,0 +1,17 @@
+package com.apachekafka.libraryeventsproducer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data // generate all getters and setters
+@Builder
+public class LibraryEvent {
+
+    private Integer libraryEventId;
+
+    private Book book;
+}
