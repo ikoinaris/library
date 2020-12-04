581bda3a05e83b0475338f459d6c5d9824260253
ikoinaris <g.koinaris@hotmail.com>
2020-12-04 10:46:01 +0100

    First Integration test

---
+++ library-events-producer/src/test/java/intg/com/apachekafka/libraryeventsproducer/controller/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,43 @@
+package com.apachekafka.libraryeventsproducer.controller;
+
+import com.apachekafka.libraryeventsproducer.domain.Book;
+import com.apachekafka.libraryeventsproducer.domain.LibraryEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LibraryEventsControllerIntegrationTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    void postLibraryEvent() {
+
+        // given
+        Book book = Book.builder()
+                .id(123)
+                .author("Ioannis Koinaris")
+                .title("Kafka using SpringBoot").build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .id(null)
+                .book(book).build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> entity = new HttpEntity<>(libraryEvent);
+
+        // when
+        ResponseEntity<LibraryEvent> responseEntity =
+                testRestTemplate.exchange("/vi/libraryevent", HttpMethod.POST, entity, LibraryEvent.class);
+
+        // then
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
+}
