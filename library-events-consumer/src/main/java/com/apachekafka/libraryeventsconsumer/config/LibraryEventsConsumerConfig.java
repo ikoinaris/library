e8c443e468c0f4729fde7e9428c0d50cd835de9e
ikoinaris <g.koinaris@hotmail.com>
2020-12-08 12:55:58 +0100

    Configure KafkaConsumer

---
+++ library-events-consumer/src/main/java/com/apachekafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.apachekafka.libraryeventsconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
