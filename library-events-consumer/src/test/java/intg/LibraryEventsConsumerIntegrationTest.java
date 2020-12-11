aeeafa413ad05241c614701169a430323d27f8ba
ikoinaris <g.koinaris@hotmail.com>
2020-12-11 11:26:32 +0100

    Integration Test for posting new Event

--- library-events-consumer/src/test/java/intg/LibraryEventsConsumerIntegrationTest.java
+++ library-events-consumer/src/test/java/intg/LibraryEventsConsumerIntegrationTest.java
@@ -1,6 +1,15 @@
+import com.apachekafka.libraryeventsconsumer.consumer.LibraryEventsConsumer;
+import com.apachekafka.libraryeventsconsumer.entity.LibraryEvent;
+import com.apachekafka.libraryeventsconsumer.jpa.LibraryEventsRepository;
+import com.apachekafka.libraryeventsconsumer.service.LibraryEventsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.MessageListenerContainer;
@@ -8,6 +17,16 @@ import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @EmbeddedKafka(topics = {"library-events"}, partitions = 3)
@@ -24,10 +43,47 @@ public class LibraryEventsConsumerIntegrationTest {
     //@Autowired
     KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
 
+    //@Autowired
+    LibraryEventsRepository libraryEventsRepository;
+
+    @SpyBean
+    LibraryEventsConsumer libraryEventsConsumer;
+
+    @SpyBean
+    LibraryEventsService libraryEventsService;
+
     @BeforeEach
     void setUp() {
         for (MessageListenerContainer messageListenerContainer: kafkaListenerEndpointRegistry.getListenerContainers()) {
             ContainerTestUtils.waitForAssignment(messageListenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
         }
+    }
+
+    @AfterEach
+    void tearDown() {
+        libraryEventsRepository.deleteAll();
+    }
+
+    @Test
+    void publishNewLibraryEvent() throws ExecutionException, InterruptedException, JsonProcessingException {
+
+        // GIVEN
+        String json = "{\"libraryEventId\":123,\"libraryEventType\":\"UPDATE\",\"book\":{\"bookId\":456,\"bookName\":\"Kafka using Spring Boot\",\"bookAuthor\":\"Ioannis Koinaris\"}}";
+        kafkaTemplate.sendDefault(json).get();
+
+        // WHEN
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await(3, TimeUnit.SECONDS);
+
+        // THEN
+        verify(libraryEventsConsumer, times(1)).onMessage(isA(ConsumerRecord.class));
+        verify(libraryEventsService, times(1)).processLibraryEvent(isA(ConsumerRecord.class));
+
+        List<LibraryEvent> libraryEventList = (List<LibraryEvent>) libraryEventsRepository.findAll();
+        assert libraryEventList.size() ==1;
+        libraryEventList.forEach(libraryEvent -> {
+            assert libraryEvent.getLibraryEventId()!=null;
+            assertEquals(456, libraryEvent.getBook().getBookId());
+        });
     }
 }
