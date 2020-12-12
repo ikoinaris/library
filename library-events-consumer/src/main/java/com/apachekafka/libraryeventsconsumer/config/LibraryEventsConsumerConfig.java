cba68ca761d3ddc97c1e8fd1949595efc0445bba
ikoinaris <g.koinaris@hotmail.com>
2020-12-12 17:25:43 +0100

    retry in Kafka Consumer

--- library-events-consumer/src/main/java/com/apachekafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/apachekafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -10,6 +10,10 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @EnableKafka
@@ -27,6 +31,24 @@ public class LibraryEventsConsumerConfig {
         factory.setErrorHandler(((thrownException, data) -> {
             log.info("Exception in consumerConfig is {} and the record is {}", thrownException.getMessage(), data);
         }));
+        factory.setRetryTemplate(retryTemplate());
         return factory;
+    }
+
+    private RetryTemplate retryTemplate() {
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        return retryTemplate;
+    }
+
+    private RetryPolicy retryPolicy() {
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+        return simpleRetryPolicy;
     }
 }
